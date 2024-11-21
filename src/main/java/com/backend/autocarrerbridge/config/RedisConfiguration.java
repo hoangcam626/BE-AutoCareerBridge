b71517e6a6a2a7ba5c97d0826e735aac899430fc
baowork <99949277+baoquoc29@users.noreply.github.com>
2024-11-21 14:28:04 +0700

    Fix

--- src/main/java/com/backend/autocarrerbridge/config/RedisConfiguration.java
+++ src/main/java/com/backend/autocarrerbridge/config/RedisConfiguration.java
@@ -1,7 +1,4 @@
 package com.backend.autocarrerbridge.config;
-
-
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
