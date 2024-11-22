559adceb2fe4e5a2d477883595cfd6393ca3406c
Le Hoang Long <hoanglong2002.jvb@gmail.com>
2024-11-22 09:15:06 +0700

    update coded

--- src/main/java/com/backend/autocarrerbridge/exception/GlobalExceptionHandle.java
+++ src/main/java/com/backend/autocarrerbridge/exception/GlobalExceptionHandle.java
@@ -42,17 +42,5 @@ public class GlobalExceptionHandle {
     apiException.setMessage(errorCode.getMessage());
     return ResponseEntity.status(errorCode.getHttpStatus()).body(apiException);
   }
-<<<<<<< HEAD
-//  @ExceptionHandler(MethodArgumentNotValidException.class)
-//  public ResponseEntity<ApiException> handleValidationExceptions(MethodArgumentNotValidException ex) {
-//    // Lấy lỗi đầu tiên từ danh sách lỗi
-//    FieldError fieldError = (FieldError) ex.getBindingResult().getAllErrors().get(0);
-//    String errorMessage = fieldError.getDefaultMessage(); // Lấy thông báo lỗi
-//
-//    // Trả về thông báo lỗi duy nhất
-//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiException().setCode(404).setMessage(errorMessage));
-//  }
-=======
->>>>>>> a85fd4ae2b70804ee75aa1abfe4ea86cc64d22ee
 
 }
