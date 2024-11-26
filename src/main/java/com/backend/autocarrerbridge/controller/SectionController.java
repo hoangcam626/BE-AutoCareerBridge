8a45154352169750f7df671f6d38fab4b37a92dd
Le Hoang Long <hoanglong2002.jvb@gmail.com>
2024-11-26 08:28:21 +0700

    update security filler cros

--- src/main/java/com/backend/autocarrerbridge/controller/SectionController.java
+++ src/main/java/com/backend/autocarrerbridge/controller/SectionController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/section")
 @RequiredArgsConstructor
 public class SectionController {
